51dd4b302791ff46b0722e3aa3e4c970a56ed3eb
MichaelQQ <mingsian.tu@vpon.com>
2016-04-27 15:58:58 +0800

    add server

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === '<validation_token>') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
